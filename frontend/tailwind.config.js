c47f6e81ac512842c6462697aa2afb73ac529262
TimSgr <timseeger2000@gmail.com>
2025-01-09 10:23:02 +0000

    fix build and structure

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,30 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        darkpurple: "#1E152A",
+        error: "#EF2D56",
+        white: "#FFFFFF",
+        success: "#7BD389",
+        warning: "#FFFC31",
+        text: "#182825",
+        blue: "#3D3B8E",
+        darkblue: "#24234C",
+      },
+    },
+  },
+  "compilerOptions": {
+    "forceConsistentCasingInFileNames": true
+  },
+  plugins: [],
+} satisfies Config;
+
